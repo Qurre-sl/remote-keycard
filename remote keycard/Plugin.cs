27857f3058747eeb49cbe53f12711f981f9b5e8f
fydne <serverafydne@gmail.com>
2020-12-19 14:44:18 +0500

    ⠀

---
+++ remote keycard/Plugin.cs
@@ -0,0 +1,116 @@
+﻿#pragma warning disable CS0618
+using Qurre.API;
+using Qurre.API.Events;
+using System.Linq;
+namespace remote_keycard
+{
+    public class Plugin : Qurre.Plugin
+    {
+        #region override
+        public override string name => "remote keycard";
+        public override void Enable() => RegisterEvents();
+        public override void Disable() => UnregisterEvents();
+        public override void Reload() { }
+        #endregion
+        #region events
+        private void RegisterEvents()
+		{
+			Qurre.Events.Player.InteractDoor += RunOnDoorOpen;
+			Qurre.Events.Player.InteractLocker += LockerInteraction;
+			Qurre.Events.Player.InteractGenerator += GenOpen;
+		}
+        private void UnregisterEvents()
+		{
+			Qurre.Events.Player.InteractDoor -= RunOnDoorOpen;
+			Qurre.Events.Player.InteractLocker -= LockerInteraction;
+			Qurre.Events.Player.InteractGenerator -= GenOpen;
+		}
+		#endregion
+		#region main
+		public void RunOnDoorOpen(InteractDoorEvent ev)
+		{
+			if (ev.Player.GetTeam() == Team.SCP) return;
+			if (!ev.IsAllowed)
+			{
+				var playerIntentory = ev.Player.inventory.items;
+				foreach (var item in playerIntentory)
+				{
+					var gameItem = UnityEngine.Object.FindObjectOfType<Inventory>().availableItems.FirstOrDefault(i => i.id == item.id);
+					if (gameItem == null)
+						continue;
+					if (gameItem.permissions == null || gameItem.permissions.Length == 0)
+						continue;
+					foreach (var itemPerm in gameItem.permissions)
+					{
+						if (itemPerm == ev.Door.permissionLevel)
+						{
+							ev.IsAllowed = true;
+							continue;
+						}
+					}
+				}
+			}
+		}
+		public void LockerInteraction(InteractLockerEvent ev)
+		{
+			if (!ev.IsAllowed)
+			{
+				if (ev.Player.GetTeam() == Team.SCP) return;
+				var playerIntentory = ev.Player.inventory.items;
+				bool chcb = false;
+				bool lvl2per = false;
+				foreach (var item in playerIntentory)
+				{
+					var gameItem = UnityEngine.Object.FindObjectOfType<Inventory>().availableItems.FirstOrDefault(i => i.id == item.id);
+					if (gameItem == null)
+						continue;
+					if (gameItem.permissions == null || gameItem.permissions.Length == 0)
+						continue;
+					foreach (var itemPerm in gameItem.permissions)
+					{
+						if (itemPerm == "PEDESTAL_ACC")
+						{
+							ev.IsAllowed = true;
+							continue;
+						}
+						if (itemPerm == "CHCKPOINT_ACC")
+						{
+							chcb = true;
+						}
+						if (itemPerm == "CONT_LVL_2")
+						{
+							lvl2per = true;
+						}
+					}
+					if (chcb && lvl2per)
+					{
+						ev.IsAllowed = true;
+						continue;
+					}
+				}
+			}
+		}
+		public void GenOpen(InteractGeneratorEvent ev)
+		{
+			if (ev.Player.GetTeam() == Team.SCP) return;
+			var playerIntentory = ev.Player.inventory.items;
+			foreach (var item in playerIntentory)
+			{
+				var gameItem = UnityEngine.Object.FindObjectOfType<Inventory>().availableItems.FirstOrDefault(i => i.id == item.id);
+				if (gameItem == null)
+					continue;
+				if (gameItem.permissions == null || gameItem.permissions.Length == 0)
+					continue;
+				foreach (var itemPerm in gameItem.permissions)
+				{
+					if (itemPerm == "ARMORY_LVL_2")
+					{
+						ev.IsAllowed = true;
+						continue;
+					}
+				}
+			}
+		}
+        #endregion
+    }
+}
---
+++ remote keycard/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("remote keycard")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("fydne")]
+[assembly: AssemblyProduct("remote keycard")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5c3c18f4-c9e0-4090-b30a-e910d94ec369")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
