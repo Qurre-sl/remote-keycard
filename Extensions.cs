41e55e0a167cec0478825d1b78c3785446457d05
AlexanderK <73738345+AlexanderK666@users.noreply.github.com>
2023-01-28 17:32:20 +0300

    v2.0.0

---
+++ Extensions.cs
@@ -0,0 +1,14 @@
+﻿using Qurre.API;
+using Qurre.API.Controllers;
+using System.Collections.Generic;
+
+namespace RemoteKeycard
+{
+	internal static class Extensions
+	{
+		public static IReadOnlyCollection<Item> GetItems(this Player player)
+		{
+			return player?.Inventory?.Items?.Values;
+		}
+	}
+}
