c280b2734dedbf3bf326fc3842419ded13dd8217
AlexanderK <73738345+AlexanderK666@users.noreply.github.com>
2023-01-28 17:55:54 +0300

    v2.0.0 [reformatted code]

--- RemoteKeycard/Plugin.cs
+++ RemoteKeycard/Plugin.cs
@@ -11,19 +11,19 @@ using Qurre.Events.Structs;
 
 namespace RemoteKeycard
 {
-	[PluginInit("RemoteKeycard", "Qurre Team", "2.0.0")]
-	internal static class Plugin
-	{
-		[EventMethod(PlayerEvents.InteractDoor)]
-		public static void OnInteractDoorEvent(IBaseEvent eventBase)
-		{
-			if (eventBase is InteractDoorEvent interactDoorEvent && interactDoorEvent.Allowed == false)
+    [PluginInit("RemoteKeycard", "Qurre Team", "2.0.0")]
+    internal static class Plugin
+    {
+        [EventMethod(PlayerEvents.InteractDoor)]
+        public static void OnInteractDoorEvent(IBaseEvent eventBase)
+        {
+            if (eventBase is InteractDoorEvent interactDoorEvent && interactDoorEvent.Allowed == false)
             {
                 DoorPermissions doorPermissions = interactDoorEvent.Door?.Permissions;
                 RoleInfomation roleInformation = interactDoorEvent.Player?.RoleInfomation;
 
                 if (Equals(doorPermissions?.RequiredPermissions, KeycardPermissions.None) ||
-                    Equals(roleInformation?.Team, Team.SCPs))
+                Equals(roleInformation?.Team, Team.SCPs))
                 {
                     return;
                 }
@@ -36,40 +36,40 @@ namespace RemoteKeycard
                     if (doorPermissions.CheckPermissions(item.Base, interactDoorEvent.Player?.ReferenceHub))
                     {
                         interactDoorEvent.Allowed = true;
-						return;
+                        return;
                     }
                 }
             }
-		}
+        }
 
-		[EventMethod(PlayerEvents.InteractLocker)]
-		public static void OnInteractLockerEvent(IBaseEvent eventBase)
-		{
-			if (eventBase is InteractLockerEvent interactLockerEvent && interactLockerEvent.Allowed == false)
-			{
-				Player player = interactLockerEvent.Player;
+        [EventMethod(PlayerEvents.InteractLocker)]
+        public static void OnInteractLockerEvent(IBaseEvent eventBase)
+        {
+            if (eventBase is InteractLockerEvent interactLockerEvent && interactLockerEvent.Allowed == false)
+            {
+                Player player = interactLockerEvent.Player;
 
                 if (Equals(player?.RoleInfomation?.Team, Team.SCPs))
-				{
-					return;
-				}
+                {
+                    return;
+                }
 
                 foreach (Item item in player?.GetItems())
                 {
                     if (item is Keycard keycard &&
-                        keycard.Permissions.HasFlagFast(KeycardPermissions.ContainmentLevelTwo) &&
-                        keycard.Permissions.HasFlagFast(KeycardPermissions.Checkpoints))
+                    keycard.Permissions.HasFlagFast(KeycardPermissions.ContainmentLevelTwo) &&
+                    keycard.Permissions.HasFlagFast(KeycardPermissions.Checkpoints))
                     {
                         interactLockerEvent.Allowed = true;
                         return;
                     }
                 }
-			}
-		}
+            }
+        }
 
         [EventMethod(PlayerEvents.InteractGenerator)]
         public static void OnInteractGeneratorEvent(IBaseEvent eventBase)
-		{
+        {
             if (eventBase is InteractGeneratorEvent interactGeneratorEvent && !Equals(interactGeneratorEvent.Status, GeneratorStatus.Unlock))
             {
                 Player player = interactGeneratorEvent?.Player;
@@ -82,13 +82,13 @@ namespace RemoteKeycard
                 foreach (Item item in player?.GetItems())
                 {
                     if (item is Keycard keycard &&
-                        keycard.Permissions.HasFlagFast(KeycardPermissions.ArmoryLevelTwo))
+                    keycard.Permissions.HasFlagFast(KeycardPermissions.ArmoryLevelTwo))
                     {
                         interactGeneratorEvent.Allowed = true;
                         return;
                     }
                 }
             }
-		}
-	}
+        }
+    }
 }
